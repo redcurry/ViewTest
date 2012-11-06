edc1dee1f95863e072c4e4cd151f1ef975eef96c
Carlos Anderson <carlosja@msu.edu>
2012-11-06 00:52:46 -0500

    Changed implementation to use color filter (much faster).

--- QuaintImageViewer/src/com/quaintsoft/imageviewer/color/ColorBalance.java
+++ QuaintImageViewer/src/com/quaintsoft/imageviewer/color/ColorBalance.java
@@ -1,63 +1,26 @@
 package com.quaintsoft.imageviewer.color;
 
-import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 
-public class ColorBalance extends BitmapColorChangerByPixelFunction {
-	
-	protected int[] red_values;
-	protected int[] green_values;
-	protected int[] blue_values;
-	
-	private int red = 0;
-	private int green = 0;
-	private int blue = 0;
-	
-	private int color = 0;
-	
-	@Override
-	protected void applyTo256Values() {
-		red_values = getAppliedTo256Values(red);
-		green_values = getAppliedTo256Values(green);
-		blue_values = getAppliedTo256Values(blue);
-	}
-	
-	protected int[] getAppliedTo256Values(int c) {
-		color = c;
-		int[] values = new int[256];
-		for (int i = 0; i < values.length; i++)
-			values[i] = pixelFunction(i);
-		return values;
-	}
+public class ColorBalance extends BitmapColorChangerByColorFilter {
 
-	public int pixelFunction(int value) {
-		return clamp(value + color);
+	private int red, green, blue;
+	
+	public ColorMatrixColorFilter colorFilter() {
+		float[] colorMatrix = {
+				1.0f, 0.0f, 0.0f, 0.0f, red,
+				0.0f, 1.0f, 0.0f, 0.0f, green,
+				0.0f, 0.0f, 1.0f, 0.0f, blue,
+				0.0f, 0.0f, 0.0f, 1.0f, 0.0f };
+		return new ColorMatrixColorFilter(new ColorMatrix(colorMatrix));
 	}
 	
-	private int clamp(float x) {
-		if (x > 255)
-			return 255;
-		else if (x < 0)
-			return 0;
-		else
-			return (int)x;
-	}
-	
-	@Override
-	protected int applyToPixel(int pixel) {
-		int r = Color.red(pixel);
-		int g = Color.green(pixel);
-		int b = Color.blue(pixel);
-		int newR = red_values[r];
-		int newG = green_values[g];
-		int newB = blue_values[b];
-		return Color.rgb(newR, newG, newB);
-	}
-
 	public void setRed(int r) {
 		red = r;
 	}
 	
-	public void setGreen(int g)	{
+	public void setGreen(int g) {
 		green = g;
 	}
 	
